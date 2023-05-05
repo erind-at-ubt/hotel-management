67cbe500321e3c12f84056c874f444ab4843f946
erind-at-ubt <eb58503@ubt-uni.net>
2023-05-05 16:26:10 +0200

    some modifications in folder

---
+++ auth/dbconn.php
@@ -0,0 +1,23 @@
+<?php
+
+// Set database credentials
+$servername = "localhost"; // replace with your actual IP address
+$username = "root";
+$password = "";
+$dbname = "hotel-management";
+
+// Create a PDO connection
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    header("Location: /hotel-management/index.php");
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// Close the connection
+$conn = null;
+
+?>
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
+<?php include 'includes/header.php';?>
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hotel Management System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap" rel="stylesheet">
   </head>
   <body>
 
@@ -31,7 +30,7 @@
           
         </div>
         <div class="col d-flex align-items-center">
-          <i class="fa-solid fa-user" id="username-icon"></i>
+          <a href="main/register.php"><i class="fa-solid fa-user" id="username-icon"></i></a>
           <div class="d-flex flex-column align-items-center justify-content-center" id="user-content">
               <h5 id="username-text">username</h5>
               <p id="job-position">Job Position</p>
@@ -171,10 +170,7 @@
 
 
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/19e928f6d9.js" crossorigin="anonymous"></script>
-    <script src="scripts/script.js"></script>
-    <script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+    
 
 </body>
 </html>
